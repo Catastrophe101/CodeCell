3e3a7feb7b454942d8662963fb54bf7dc897e35a
Nishit <nishitahuja@gmail.com>
2019-04-26 08:19:56 +0530

    repairing the broken

---
+++ database/migrations/2019_04_26_023839_create_notices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNoticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+			$table->string('title');
+			$table->string('link');
+			$table->string('platform');
+			$table->string('start_date');
+			$table->string('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}
